46d378a7bce3282900963323f0eabe62dd008342
HK560 <lan2496846801@gmail.com>
2021-09-22 16:59:18 +0800

    完善项目

---
+++ 下位机/ESP8266PCReadBeta2.0/ESP8266PCRead2.0/ESP8266PCRead2.0.ino
@@ -0,0 +1,195 @@
+//兼容上位机版本0.2
+//下位机程序版本ver2.0
+//版本说明：支持播放动画
+#include <U8g2lib.h>
+#include <Wire.h>
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+
+
+String dataName="";
+String trueName="";
+String dataValue="";
+String unit;
+String type="";
+byte revByte;
+char ouputName[15],ouputData[15],ouputUnit[4];
+int couttime=0;
+uint8_t frame[1024] = {};
+
+struct dataList{
+  String dataName;
+  String trueName;
+  String unit;
+};
+
+dataList hwList[10]={
+  {"CPU_Volt","CPU Voltage","V"},
+  {"GPU_Volt","GPU Voltage","V"},
+  {"CPU_UTI","CPU Usage","%"},
+  {"CPU_TMP","CPU Temp","C"},
+  {"CPU_PKG","CPU Package","W"},
+  {"CPU_CLK","CPU Clock","MHz"},
+  {"GPU_UTI","GPU Usage","%"},
+  {"GPUMC_UTI","GPU Mem Usage","%"},
+  {"GPU_TMP","GPU Temp","C"},
+  {"MEM_UTI","Memory Usage","%"},
+};
+
+void refresh(){//显示函数
+    //u8g2.clear(); 
+    Serial.println("display");
+    u8g2.clearBuffer();                         // 清空显存
+    //u8g2.setFont(u8g2_font_unifont_t_chinese2);
+    u8g2.setFont(u8g2_font_missingplanet_tr);
+    u8g2.drawStr((128-(u8g2.getUTF8Width("My PC Info")))/2, 10, "My PC Info");
+    u8g2.drawStr((128-(u8g2.getUTF8Width(ouputName)))/2, 25, ouputName);
+    
+    u8g2.setFont(u8g2_font_freedoomr25_tn);
+    u8g2.drawStr((128-(u8g2.getUTF8Width(ouputData)))/2, 60, ouputData);
+    int length=((128-(u8g2.getUTF8Width(ouputData)))/2)+u8g2.getUTF8Width(ouputData);
+    u8g2.setFont(u8g2_font_missingplanet_tf);
+    if(strcmp(ouputUnit,"C")==0){//解决显示不了温度单位
+      u8g2.setCursor(length+6, 60);
+      char c=(char)176;//修改字体需要参考字符文档
+      u8g2.print(c);
+      u8g2.print(ouputUnit);
+    }else{
+    u8g2.drawStr(length+6, 60, ouputUnit);
+    }
+    u8g2.sendBuffer();
+
+}
+
+void boot(){//没有收到数据时候执行的函数
+  String title="elbadaernU";//可自定义，限制长度为15
+  String subtitle="Waiting for signal";//可自定义，限制长度为15
+  char titleChar[15],subtitleChar[30];
+  title.toCharArray(titleChar,15);
+  subtitle.toCharArray(subtitleChar,30);
+  
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_missingplanet_tr);
+  //设置字体库可以查询u8g2库wiki：https://github.com/olikraus/u8g2/wiki/fntlistall
+  u8g2.drawStr((128-(u8g2.getUTF8Width(titleChar)))/2, 20, titleChar);
+  u8g2.drawStr((128-(u8g2.getUTF8Width(subtitleChar)))/2, 40, subtitleChar);
+  u8g2.sendBuffer();
+}
+
+void setTrueName(String dataName, String& trueName,String& unit){
+  for(int i=0;i<10;i++){
+    if(dataName==hwList[i].dataName){
+      trueName=hwList[i].trueName;
+      unit=hwList[i].unit;
+      return;
+    }
+  }
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+    u8g2.begin();
+    u8g2.enableUTF8Print();
+    u8g2.clearBuffer();
+    Serial.begin(1500000);
+    Serial.println("Ready");
+    boot();
+}
+//!DA#CPU=5?
+void loop() {
+  // put your main code here, to run repeatedly:
+    
+    if(Serial.available()>0){
+      u8g2.clearBuffer();
+        revByte = Serial.read();
+        if (revByte == '@'){
+            if (Serial.available() > 0){
+                revByte = Serial.read();
+                if (revByte == '$'){
+                    //int countnum = 0;
+                    for (int i = 0; i < 1024; i++){
+                        while (Serial.available() == 0)
+                            ;
+                        frame[i] = Serial.read();
+                        //countnum++;
+                    }
+                    u8g2.firstPage();
+                    do{
+                        u8g2.drawXBM(0, 0, 128, 64, frame);
+                    } while (u8g2.nextPage());
+                }
+                else if (revByte == '&')
+                    return;
+            }
+        }else{
+            if (revByte=='!'){
+                //Serial.println("read");
+                while(revByte!='#'){
+                    while (Serial.available() == 0)
+                    ;
+                    revByte=Serial.read();
+                    if(revByte!='#'){
+                    type+=(char)revByte;
+                    }
+                }
+                 //Serial.println("readName");
+                while(revByte!='='){
+                    while (Serial.available() == 0)
+                    ;
+                    revByte=Serial.read();
+                    if(revByte!='='){
+                    dataName+=(char)revByte;
+                    }
+                }
+                 //Serial.println("readvalue");
+                while(revByte!='?'){
+                    while (Serial.available() == 0)
+                    ;
+                    revByte=Serial.read();
+                    if(revByte!='?'){
+                    dataValue+=(char)revByte;
+                    }
+                }
+            
+                Serial.println(type); //如果需要显示其他的信息，去上位机输出里查名称
+                Serial.println(dataName);
+                Serial.println(dataValue);
+                //strcpy(ouputName,dataName);
+                //strcpy(ouputData,dataValue);
+                //ouputName=const_cast<char*>(dataName.c_str());
+                //ouputData=const_cast<char*>(dataValue.c_str());
+
+                setTrueName(dataName,trueName,unit);
+                trueName.toCharArray(ouputName,15);//数据名字传到outputname
+                dataValue.toCharArray(ouputData,15);//数据值传到outputdata
+                unit.toCharArray(ouputUnit,4);
+            
+                dataName="";//清除
+                trueName="";
+                dataValue="";
+                type="";
+            
+                refresh();//执行刷新显示函数
+            }
+            couttime=0;
+            //Serial.println("revmsg");
+            revByte=Serial.read(); //读！
+            //Serial.print("FRead，should be ！ ");
+            //Serial.println((char)revByte);
+            while(Serial.read()>=0){}
+        }
+
+    }
+    // else{ 
+
+    //   //如果5s内没有收到数据则执行boot()函数
+    //     if(couttime!=5){
+    //         couttime+=1;
+    //         delay(1000);
+    //     }else{
+    //         boot();
+    //         couttime=0;
+    //     }
+    
+    // }
+}
